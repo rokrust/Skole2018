5ae103fa646ffdfcc2b0b81dc097e99d5cae0a04
Rokrust <rokrust@hotmail.com>
2018-01-28 14:19:32 +0100

    add first part of exercise

---
+++ TDT4173/Exercise1/datasets/regression/regression_csvread.m
@@ -0,0 +1,10 @@
+function [ X, y ] = regression_csvread( file_name )
+    training_data = csvread(file_name);
+
+    n = size(training_data, 1);
+
+    X = [ones(n, 1), training_data(:, 1:end-1)];
+    y = training_data(:, end);
+
+end
+

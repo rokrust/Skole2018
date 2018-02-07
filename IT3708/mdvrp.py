b6451396162ccaa2e09727ff87bb58dd4e8cd1fa
Rokrust <rokrust@hotmail.com>
2018-02-07 22:13:51 +0100

    more stuff

--- IT3708/mdvrp.py
+++ IT3708/mdvrp.py
@@ -1,6 +1,6 @@
 from math import sqrt
 #import matplotlib.pyplot as plt
-from random import randrange
+from random import shuffle
 
 
 class Position:
@@ -35,8 +35,8 @@ class Data:
             self.n_vehicles = line[0]
             self.n_customers = line[1]
             self.n_depots = line[2]
-            self.distances = [[0 for x in range(self.n_customers)] for y in range(self.n_customers)]
-            self.distances_depot = [[0 for x in range(self.n_customers)] for y in range(self.n_depots)]
+            self.distance = [[0 for x in range(self.n_customers)] for y in range(self.n_customers)]
+            self.distance_depot = [[0 for x in range(self.n_customers)] for y in range(self.n_depots)]
 
             # Store the depot specific data
             for t in range(self.n_depots):
@@ -61,42 +61,28 @@ class Data:
         #Calculate distance matrix
         for n_row in range(self.n_customers):
             for n_col in range(n_row):
-                self.distances[n_row][n_col] = dist(self.customer_positions[n_row], self.customer_positions[n_col])
-                self.distances[n_col][n_row] = self.distances[n_row][n_col]
+                self.distance[n_row][n_col] = dist(self.customer_positions[n_row], self.customer_positions[n_col])
+                self.distance[n_col][n_row] = self.distance[n_row][n_col]
 
             for depot in range(self.n_depots):
-                self.distances_depot[depot][n_row] = dist(self.customer_positions[n_row], self.depot_positions[depot])
+                self.distance_depot[depot][n_row] = dist(self.customer_positions[n_row], self.depot_positions[depot])
 
-# GA classes
-class Genotype:
-    depot_nbr = 0
-    car_nbr = 0
-    route = []
 
-    def __init__(self, car_nbr, depot_nbr, route):
-        self.depot_nbr = depot_nbr
-        self.car_nbr = car_nbr
-        self.route = list(route)
-
-    def _feasible(self, total_distance, max_distance, capacity, max_capacity):
-        return total_distance < max_distance and capacity < max_capacity
 
 # A single solution to the the problem
 class Phenotype:
     routes = []
-    def __init__(self, n_depots, n_cars):
-        pass
+    def __init__(self, n_depots, n_vehicles):
+        self.routes = [[[] for x in range(n_vehicles) ] for y in range(n_depots)]
 
-    @classmethod
     def greedy_init(self, data):
-
         # Group customers to their closest depot
         prev_distance = 9999999
         closest_depot = 0
         grouped_customers = [[] for x in range(data.n_depots)]
         for customer in range(data.n_customers):
             for depot in range(data.n_depots):
-                current_distance = data.distances_depot[depot][customer]
+                current_distance = data.distance_depot[depot][customer]
                 if current_distance < prev_distance:
                     closest_depot = depot
                     prev_distance = current_distance
@@ -105,43 +91,49 @@ class Phenotype:
 
         # Initialize population through a greedy approach
         for depot in range(data.n_depots):
-            for vehicle in range(data.n_vehicles):
-                leftover_duration = data.depot_data[depot][0]
-                leftover_capacity = data.depot_data[depot][1]
+            shuffle(grouped_customers[depot])
 
-                if leftover_duration == 0:
-                    leftover_duration = 9999999
-                if leftover_capacity == 0:
-                    leftover_capacity = 9999999
+            while len(grouped_customers[depot]) > 0:
+                for vehicle in range(data.n_vehicles):
+                    self.routes[depot][vehicle].append(grouped_customers[depot].pop())
 
-                route = [0]
-                    i = randrange(0, len(grouped_customers[depot]))
-                    route.append(grouped_customers[depot][i])
-                    del grouped_customers[depot][i]
-
-                    leftover_duration -= data.customer_data[customer][0] # duration from driving distance?
-                    leftover_capacity -= data.customer_data[customer][1] ## FIX THIS
-                route.append(0)
-                self.routes[depot][vehicle] = Genotype(vehicle, depot, route)
+                    if len(grouped_customers[depot]) == 0:
+                        break
 
     def fitness(self, data):
-        # Calculate total route distance
+        # Calculate total route distance for every vehicle
         total_distance = 0
-        previous_customer = 0
-        for customer in self.route[1:]:
-            total_distance += data.distances[customer][previous_customer]
-            previous_customer = customer
+        for depot in range(data.n_depots):
+            for vehicle in range(data.n_vehicles):
+                # Distance from depot to customer
+                prev_customer = self.routes[depot][vehicle][0]
+                route_distance = data.distance_depot[depot][prev_customer]
+                route_duration = data.customer_data[prev_customer][0]
+                route_demand = data.customer_data[prev_customer][1]
 
-        # Penalize infeasibility
-        # Put in another function
-        depot_data = data.depot_data[self.depot_nbr]
-        max_distance = depot_data[0]
-        max_capacity = depot_data[1]
-        capacity = 0
-        if not self._feasible(total_distance, max_distance, capacity, max_capacity):
-            total_distance += 100
+                for customer in self.routes[depot][vehicle][1:]:
+                    route_distance += data.distance[customer][prev_customer]
+                    prev_customer = customer
+                    route_duration += data.customer_data[customer][0]
+                    route_demand += data.customer_data[customer][1]
+
+                # Penalize exceeding of constraints
+                if route_duration > data.depot_data[depot][0] and data.depot_data[depot][0] != 0:
+                    route_distance += 100
+
+                if route_demand > data.depot_data[depot][1] and data.depot_data[depot][1] != 0:
+                    route_distance += 100
+
+                route_distance += data.distance_depot[depot][prev_customer]
+                total_distance += route_distance
 
         return total_distance
+
+    def _mutate(self):
+        pass
+
+    def _recombine(self):
+        pass
 
 class GeneticAlgorithm:
     # A list of individual solutions to the problem (phenotypes)
@@ -149,7 +141,11 @@ class GeneticAlgorithm:
 
     def __init__(self, data, population_size):
         for individual in range(population_size):
-            self.population.append(Phenotype.greedy_init(data))
+            self.population.append(Phenotype(data.n_depots, data.n_vehicles))
+            self.population[-1].greedy_init(data)
+
+    def create_next_generation(self):
+        pass
 
 ## Util
 def dist(pos_a, pos_b):
@@ -165,11 +161,13 @@ data = Data(path + file_name)
 population = 1000
 mutation_rate = 0.02
 crossover_rate = 0.8
-elite_individuals = 0.1
+elitism_rate = 0.1
 
 # Initialize population
-run = GeneticAlgorithm(data, 1)
-pass
+run = GeneticAlgorithm(data, 3)
+
+for i in range(3):
+    print(run.population[i].fitness(data))
 # TODO
     # make fitness function
     # make initial population

dbe141048e4ea653d7aadc46cb6f7b667cbeeaf9
Øyvind Aukrust Rones <oyvinaro@ntnu.no>
2018-04-19 18:08:27 +0200

    Initial commit assignment 3 bioai

---
+++ IT3708/Assignment 3/unit_test.py
@@ -0,0 +1,15 @@
+#Run this for unit tests
+
+import unittest
+
+class SomeTest(unittest.TestCase):
+    def test_something:
+        self.assertEqual("something", "something else")
+
+    def test_something_else:
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
+
+    
